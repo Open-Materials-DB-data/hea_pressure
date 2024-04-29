30323cd350358f7bb4622585bace28fec2988d5a
Rickard Armiento <gitcommits@armiento.net>
2024-04-29 11:21:37 +0200

    Update publish.py

--- publish.py
+++ publish.py
@@ -4,15 +4,15 @@ import os, shutil, sys
 
 from httk.httkweb import publish
 
-if not os.path.exists("output"):
-    os.mkdir("output")
+if not os.path.exists("public"):
+    os.mkdir("public")
 
-for filename in os.listdir("output"):
+for filename in os.listdir("public"):
     if not filename.startswith("."):
-        f = os.path.join("output",filename)
+        f = os.path.join("public",filename)
         if os.path.isdir(f):
             shutil.rmtree(f)
         else:
             os.unlink(f)
 
-publish("src","output",'http://127.0.0.1/')
+publish("src","public",'http://127.0.0.1/')
